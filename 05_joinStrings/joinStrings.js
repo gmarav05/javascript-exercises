3cebdc802f8fc818d285d47655d8cb0124797cea
Aravind <gmarav005@gmail.com>
2025-08-09 20:43:56 +0530

    test 5 pass

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,8 +9,14 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+const greeting = 'Hello! My name is ' + firstName + ' ' + lastName + ' and ' + 'I am 18 years old.';
 
-
+const fullName = firstName + " " + lastName;
+const age = 18;
 
 
 // Do not change this
