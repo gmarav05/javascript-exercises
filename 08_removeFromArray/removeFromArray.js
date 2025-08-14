dbaa84120a203520a6e9484e618b93536a7db8dc
Aravind <gmarav005@gmail.com>
2025-08-14 18:19:04 +0530

    js - 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+
+    let newArr = [];
+
+
+    array.forEach((item) => {
+        
+    if(!args.includes(item)) {
+        newArr.push(item);
+    }
+
+   });
+
+
+
+    return newArr;
+
+
 };
 
 // Do not edit below this line
