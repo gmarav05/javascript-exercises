165a3b45089f23598fb28ddfa64bdddc6179e8f9
Aravind <gmarav005@gmail.com>
2025-08-14 18:31:59 +0530

    js - 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+
+    if (num1 < 0 ||  num2 < 0) {
+        return "ERROR";
+    }
+
+    if (num1 > num2) {
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    let sum = 0;
+
+    for (let index = num1; index <= num2; index++) {
+
+        sum += index;
+      
+        
+    }
+
+
+    return sum;
+
 
 };
 
