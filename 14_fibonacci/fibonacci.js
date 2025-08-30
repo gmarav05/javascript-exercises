95275a53e961b982f920b432d5b4c1dbe926b67c
Aravind <gmarav005@gmail.com>
2025-08-31 00:29:04 +0530

    fibonacci  exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+
+    let count 
+
+    if (typeof  countArg !== 'number') {
+        count = parseInt(countArg)
+    } else {
+        count = countArg
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    let firstNumber = 1
+    let secondNumber = 0
+
+    for (let i = 2; i <= count; i++) {
+        let current = firstNumber + secondNumber;
+
+        secondNumber = firstNumber;
+        firstNumber = current;
+
+    }
+
+    return firstNumber;
+
 
 };
 
